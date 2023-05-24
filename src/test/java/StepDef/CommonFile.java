ce874f4e47fdd7c4e29a8452ef03088f95a26781
canantu <tunc.canan84@gmail.com>
2023-05-24 19:33:23 +0200

    updated Commonfile

--- src/test/java/StepDef/CommonFile.java
+++ src/test/java/StepDef/CommonFile.java
@@ -4,5 +4,9 @@ public class CommonFile {
     public static void main(String[] args) {
 
         System.out.println("This is common file");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i want to print this 5 times");
+        }
     }
 }
